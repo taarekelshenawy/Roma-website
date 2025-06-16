24c307ac12b7e5e3f4d31e2ef96c739a7a7d8ce0
taarekelshenawy <telshenawy562@gmail.com>
2025-06-16 20:44:09 +0300

    improvecode

--- main/main.js
+++ main/main.js
@@ -63,3 +63,43 @@ window.addEventListener("scroll", () => {
 
 
 
+$(document).ready(function(){
+    $('.slider').slick({
+        rtl:true,
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+  });
+
+
